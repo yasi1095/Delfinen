702e062367f6d7795b2b37197f5287b224690579
Yasi1095 <yasi1095@stud.kea.dk>
2022-11-29 11:57:22 +0100

    1

--- src/Medlemmer.java
+++ src/Medlemmer.java
@@ -1,12 +1,20 @@
-public class Medlemmer extends brugeroplysninger {
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
 
-    private String age = " ";
+public class Medlemmer {
+
+    private String dateOfBirth;
     private String name;
+
+    Scanner in = new Scanner(System.in);
     private boolean aktiv;
     private boolean restance;
     private boolean konkurrenceSvømmer;
     private final int seniorPris = 1600;
     private final int juniorPris = 1000;
+    private int price = 0;
+
 
     public String getName() {
         return name;
@@ -16,57 +24,79 @@ public class Medlemmer extends brugeroplysninger {
         this.name = name;
     }
 
+    public void setAktiv(boolean aktiv) {
+        this.aktiv = aktiv;
+    }
+
+    public boolean isAktiv() {
+        return aktiv;
+    }
+
+    public int getAgeInYears() {
+
+        LocalDate birthYear = LocalDate.parse(dateOfBirth);
+
+        long ageDifference = ChronoUnit.YEARS.between(birthYear, LocalDate.now());
+
+        return (int) ageDifference;
+    }
+
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
     public int getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
+    // pristjek i forhold til medlemmets status og pris heraf
+    public int prisTjek() {
 
-    private int price;
-
-    public String getAge() {
-        return getAge();
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-// pristjek i forhold til medlemmets status og pris heraf
-    public void prisTjek() {
-
-        int age = Integer.parseInt(getAge());
-        if (age >= 60) {
+        if (getAgeInYears() >= 60) {
             price = seniorPris / 100 * 75;
-        } else if (age >= 18 && age < 60) {
+        } else if (getAgeInYears() >= 18 && getAgeInYears() < 60) {
             price = seniorPris;
-        } else if (age < 18) {
+        } else if (getAgeInYears() < 18) {
             price = juniorPris;
         } else if (aktiv == false) {
             price = 500;
         }
+        return price;
     }
 
     //Aktivitetsform i forhold til konkurrencesvømmere samt om de skal på senior eller juniorholdet
-    public Svømmehold aktivitetsForm(boolean konkurrenceSvømmer){
+    public Svømmehold aktivitetsForm(boolean konkurrenceSvømmer) {
         Svømmehold s;
 
-        int age = (Integer.parseInt(getAge()));
 
-
-        if (age >= 18 && konkurrenceSvømmer == true) {
+        if (getAgeInYears() >= 18 && konkurrenceSvømmer == true) {
             s = Svømmehold.SENIOR;
-return Svømmehold.SENIOR;
-        }else  {
-                s = Svømmehold.JUNIOR;
-                return Svømmehold.JUNIOR;
-
+            return Svømmehold.SENIOR;
+        } else {
+            s = Svømmehold.JUNIOR;
+            return Svømmehold.JUNIOR;
 
         }
-
-// 
+//
     }
 
+    @Override
+    public String toString() {
+        return "Medlemmer{" +
+                "age='" + getAgeInYears() + '\'' +
+                ", name='" + name + '\'' +
+                ", aktiv=" + aktiv +
+                ", restance=" + restance +
+                ", konkurrenceSvømmer=" + konkurrenceSvømmer +
+                ", seniorPris=" + seniorPris +
+                ", juniorPris=" + juniorPris +
+                ", price=" + prisTjek() +
+                '}';
+
+    }
 }
