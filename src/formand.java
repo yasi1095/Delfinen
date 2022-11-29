c6df23c7b36644572faa10da79bdcac2a4f6a5c9
Yasi1095 <yasi1095@stud.kea.dk>
2022-11-29 12:09:49 +0100

    pushin P

--- src/formand.java
+++ src/formand.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class formand extends Medlemmer{
     public void indmeldelse(){
         Scanner in = new Scanner(System.in);
-        System.out.printf("Indtast medlems navn");
+        System.out.println("Indtast medlems navn");
         setName(in.nextLine());
 
-        System.out.printf("Indtast medlems fødselsdato i formatet ÅÅÅÅ-MM-DD");
-        setAge(in.nextLine());
+        System.out.println("Indtast medlems fødselsdato i formatet ÅÅÅÅ-MM-DD");
+        setDateOfBirth(in.nextLine());
 
-        System.out.println("Indtast medlems aktivitetsform");
+        System.out.println("Er " + getName() + " aktiv eller ej. hvis ja så skrev ja. Hvis ikke så skriv nej ");
 
+        String s = in.nextLine();
+        if (s.equalsIgnoreCase("ja")){
+            setAktiv(true);
+        } else {
+            setAktiv(false);
+        }
 
-
-
+        //System.out.println("Indtast medlems aktivitetsform");
 
     }
 
